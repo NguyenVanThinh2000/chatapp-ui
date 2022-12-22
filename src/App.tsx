c311964125797314b264842739b8fa5edfea3f7d
NguyenVanThinh2000 <18521448@gm.uit.edu.vn>
2022-12-22 18:17:59 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
+
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const socket = io("http://localhost:3001");
+
+interface IMessage {
+  name: string;
+  message: string;
+}
 
 function App() {
+  const [name, setName] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [messageList, setMessageList] = useState<IMessage[]>([]);
+  useEffect(() => {
+    setName(prompt("Enter your name") || "default");
+  }, []);
+  useEffect(() => {
+    socket.on("receive-message", (data) => {
+      setMessageList([...messageList, data.data]);
+    });
+  }, [messageList]);
+  const handleSendMessage = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const data: IMessage = {
+      name,
+      message,
+    };
+    socket.emit("send-message", {
+      data,
+    });
+
+    setMessage("");
+  };
+  const handleChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {messageList.map((mes: IMessage, index) => (
+          <li key={index}>
+            {mes.name}: {mes.message}
+          </li>
+        ))}
+      </ul>
+      <form action="" onSubmit={handleSendMessage}>
+        <input
+          onChange={handleChangeMessage}
+          type="text"
+          placeholder="Enter your message..."
+          value={message}
+        />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 }
