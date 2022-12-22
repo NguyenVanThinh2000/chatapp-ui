b6fd8a357120f705f39c2df10ac3f82a6631a00f
NguyenVanThinh2000 <18521448@gm.uit.edu.vn>
2022-12-22 22:26:07 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -6,58 +6,58 @@ import "./App.css";
 const socket = io("http://localhost:3001");
 
 interface IMessage {
-  name: string;
-  message: string;
+    name: string;
+    message: string;
 }
 
 function App() {
-  const [name, setName] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
-  const [messageList, setMessageList] = useState<IMessage[]>([]);
-  useEffect(() => {
-    setName(prompt("Enter your name") || "default");
-  }, []);
-  useEffect(() => {
-    socket.on("receive-message", (data) => {
-      setMessageList([...messageList, data.data]);
-    });
-  }, [messageList]);
-  const handleSendMessage = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+    const [name, setName] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [messageList, setMessageList] = useState<IMessage[]>([]);
+    useEffect(() => {
+        setName(prompt("Enter your name") || "default");
+    }, []);
+    useEffect(() => {
+        socket.on("receive-message", (data) => {
+            setMessageList([...messageList, data.data]);
+        });
+    }, [messageList]);
+    const handleSendMessage = (e: React.SyntheticEvent) => {
+        e.preventDefault();
 
-    const data: IMessage = {
-      name,
-      message,
+        const data: IMessage = {
+            name,
+            message,
+        };
+        socket.emit("send-message", {
+            data,
+        });
+
+        setMessage("");
     };
-    socket.emit("send-message", {
-      data,
-    });
-
-    setMessage("");
-  };
-  const handleChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMessage(e.target.value);
-  };
-  return (
-    <div className="App">
-      <ul>
-        {messageList.map((mes: IMessage, index) => (
-          <li key={index}>
-            {mes.name}: {mes.message}
-          </li>
-        ))}
-      </ul>
-      <form action="" onSubmit={handleSendMessage}>
-        <input
-          onChange={handleChangeMessage}
-          type="text"
-          placeholder="Enter your message..."
-          value={message}
-        />
-        <button type="submit">send</button>
-      </form>
-    </div>
-  );
+    const handleChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(e.target.value);
+    };
+    return (
+        <div className="App">
+            <ul>
+                {messageList.map((mes: IMessage, index) => (
+                    <li key={index}>
+                        {mes.name}: {mes.message}
+                    </li>
+                ))}
+            </ul>
+            <form action="" onSubmit={handleSendMessage}>
+                <input
+                    onChange={handleChangeMessage}
+                    type="text"
+                    placeholder="Enter your message..."
+                    value={message}
+                />
+                <button type="submit">send</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
